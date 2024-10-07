e8b11c95b5475eafbc72b43532c47b54574c712c
phucs27 <86290492+phucs27@users.noreply.github.com>
2024-10-08 00:31:00 +0700

    classification language

---
+++ split_audio.py
@@ -0,0 +1,21 @@
+from pydub import AudioSegment
+from datetime import datetime
+# Đọc file audio WAV
+audio = AudioSegment.from_wav(r"D:\0_Workplace\data_audio\vietnamese1.wav")
+
+# Độ dài mỗi đoạn (đơn vị là mili giây)
+segment_length = 5 * 1000  # 10 giây
+
+time = datetime.now()
+datetime_str = time.strftime("%Y_%m_%d_%H_%M_%S_%f")
+print(datetime_str)
+
+# Cắt và lưu file thành nhiều đoạn
+for i in range(0, len(audio), segment_length):
+    segment = audio[i:i + segment_length]
+    file_name = f"_{i // 1000}" + datetime_str + ".wav"
+    file_path = r'D:\0_Workplace\Project_Python\audio_process\data\train\vi\vi' + file_name
+    segment.export(file_path, format="wav")
+    print(file_path)
+
+print("Cắt và lưu file thành công!")
